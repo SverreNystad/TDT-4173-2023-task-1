5e04e7aa04ffd7a31ffccac9436be652a67a4c2c
Sverre Nystad <sverrnys@ntnu.no>
2023-09-15 02:07:50 +0200

    Feat: Add gain

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -44,12 +44,30 @@ class DecisionTree:
         """
 
         # 1. Calculate the entropy of every attribute a of the data set S.
+        
+
+        for attribute in range(len(X.columns)):
+            feature = X.columns[attribute]
+            print(feature)
+
+            print(gain(X, feature, y))
         # 2. Partition ("split") the set S into subsets using the attribute for which the resulting entropy after splitting is minimized; or, equivalently, information gain is maximum.
         # 3. Make a decision tree node containing that attribute.
         # 4. Recurse on subsets using the remaining attributes
 
+        # Repeatedly select the best features to split the data
+        # Stop splitting, eh. when reaching:
+        # 1. max depth
+        # 2. min number of samples for each split
+        # 3. min number of samples for each leaf
+        # 4. Maximum number of features
+        # Pruining could be applied to control complexity
+        # Inference
+        # 1. Navigate to a leaf node using the splitting 
+
         # TODO: Implement 
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        pass
     
     def predict(self, X: pd.DataFrame):
         """
@@ -106,8 +124,48 @@ def accuracy(y_true, y_pred):
     assert y_true.shape == y_pred.shape
     return (y_true == y_pred).mean()
 
+def gain(S: pd.DataFrame, A: str, y: pd.DataFrame, plus="Yes", minus="No") -> float:
+    """
+    Computes the information gain of a partitioning with respect to an attribute
 
-def entropy(counts):
+    Args:
+        S (DataFrame): a matrix with discrete value where
+                each row is a sample and the columns correspond
+                to the features.
+        A (str): attribute
+        y (DataFrame): a vector of discrete ground-truth labels
+        plus (str): the label for the positive class
+        minus (str): the label for the negative class
+    Returns:
+        A positive float scalar corresponding to the information gain
+        of the partitioning.
+    """
+    # The discrete values of the samples with respect to the attribute A
+    values = np.unique(S[A])
+    total_count = len(S)
+
+    outcomes = np.unique(y)
+    s_tot = np.zeros(len(outcomes))
+    sub_gains = []
+    for value in values:
+        plus_count = 0
+        minus_count = 0
+        for row in range(total_count):
+            if S[A].iloc[row] == value and y.iloc[row] == plus:
+                plus_count += 1
+            elif S[A].iloc[row] == value and y.iloc[row] == minus:
+                minus_count += 1
+        s_v = np.array([plus_count, minus_count])
+        s_tot += s_v
+
+        attribute_amount = np.sum(S[A]==value)
+        sub_gain = (attribute_amount/total_count) * entropy(s_v)
+        sub_gains.append(sub_gain)
+
+    total_entropy = entropy(s_tot) - sum(sub_gains)
+    return total_entropy
+
+def entropy(counts: np.ndarray):
     """
     Computes the entropy of a partitioning
     
@@ -131,3 +189,21 @@ def entropy(counts):
 
 
 
+
+if __name__ == "__main__":
+    data_1 = pd.read_csv("decision_tree\data_1.csv")
+
+    # Create and fit a Decrision Tree classifier
+    X = data_1.drop(columns=['Play Tennis'])
+    y = data_1['Play Tennis']
+    y.head()
+    temperature = X.columns[3]
+    for i in range(len(X.columns)):
+        print(X.columns[i])
+        print(gain(X, X.columns[i], y))
+    # Create and fit a Decrision Tree classifier
+    model_1 = DecisionTree()  # <-- Should work with default constructor
+    model_1.fit(X, y)
+
+    # Verify that it perfectly fits the training set
+    print(f'Accuracy: {accuracy(y_true=y, y_pred=model_1.predict(X)) * 100 :.1f}%')
